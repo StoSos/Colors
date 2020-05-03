837ba594afb502c287922f75df0f8e3c4f132c57
StoSos <64641401+StoSos@users.noreply.github.com>
2020-05-03 11:09:31 +0200

    damages
    j'ai rajouté les damages.

---
+++ Assets/Enemygivedamage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Enemygivedamage : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+        playerHealth.TakeDamage(10);
+    }
+}
